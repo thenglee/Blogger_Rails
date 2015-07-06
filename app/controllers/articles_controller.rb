85078a5cf8a3a5f3a353c9d83a3027f83ee9baa8
Tan Theng Lee <thengleetan@gmail.com>
2015-07-06 22:24:08 +0800

    Add delete article action and show edit article using form partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,10 +14,21 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
 	def create
 		@article = Article.new(article_params)
 		@article.save
 
 		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
 	end
 end
